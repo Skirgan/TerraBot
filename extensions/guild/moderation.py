3c62cae2348357e3292f374a8408346e766c6503
Skirgan <Mr.KirushaS@gmail.com>
2024-11-14 21:54:16 +0300

    Fix moderation.
    Модерация работает штатно. В 21 строке стоит except AttributeError для обработки ошибок из-за эфимерных сообщений. По неизвестной мне причине, они относятся к личным сообщениям (буквально, это message.channel.type == discord.DMChannel), но это не удаётся обрабатывать из-за непонятной мне архитектуре. Следует заняться этим моментом, когда будет время.

--- extensions/guild/moderation.py
+++ extensions/guild/moderation.py
@@ -11,9 +11,15 @@ class EventsModeration(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.category_id == forum_tasks_id and message.content.startswith("(("):
-            await asyncio.sleep(20)
-            await message.delete()
+        try:
+            if message.channel.category_id == forum_tasks_id:
+               for message_in_channel_history in message.channel.history(oldest_first = True):
+                   if message_in_channel_history.content.startswith("((") and message_in_channel_history.author == message.author:
+                       await asyncio.sleep(10)
+                       await message_in_channel_history.delete()
+                       break
+        except AttributeError:
+            pass
 
 def setup(bot):
     bot.add_cog(EventsModeration(bot))
